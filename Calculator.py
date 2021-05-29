f6a4f053fe4693d26a4cbeeffaae1c894d77840f
pacarti <59978338+pacarti@users.noreply.github.com>
2021-05-29 21:08:46 +0200

    Update Calculator.py
    Now it works in it's basic way.

--- Calculator.py
+++ Calculator.py
@@ -1,9 +1,29 @@
 import sys
 
-print("Hello! Welcome in a simple calculator program. Please choose the operation you want to make: \n + to add \n - to subtract \n * to multiply \n : to divide \n % to modulo \n \ to divide and round up to intiger. \n\n 0 to exit:", end = ' ')
+print("Hello! Welcome in a simple calculator program. \nPlease enter first number: ", end = '')
+a = int(input())
+print("Please enter second number: ", end = '')
+b = int(input())
+
+print(a)
+print(b)
+print("Please choose the operation you want to make. 0 to exit: \n + to add \n - to subtract \n * to multiply \n : to divide \n % to modulo \n / to divide and round up to intiger.:", end = ' ')
 
 operator = input()
 
 if operator == '0':
     print("Goodbye. See you soon.")
     sys.exit()
+elif operator == '+':
+    result = a + b
+elif operator == '-':
+    result = a - b
+elif operator == '*':
+    result = a * b
+elif operator == ':':
+    result = a / b
+elif operator == '%':
+    result = a % b
+elif operator == '/':
+    result = a // b
+print(f"Result is: {result}")
